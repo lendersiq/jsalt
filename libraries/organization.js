8c82c4e503176221ea78ec864f6bd6c649f7a9ab
lendersiq <lendersiq@gmail.com>
2024-08-11 21:22:52 -0400

    Add files via upload
    initial commit

---
+++ libraries/organization.js
@@ -0,0 +1,9 @@
+const organization = {
+    attributes: {
+        taxRate: {
+            description: "The institution tax rate applied to financial calculations",
+            value: 0.27
+        },
+    }
+};
+window.organization = organization;
